f5a8f66df6f4a86c77b0e9d77f02c5d7408e0ba8
unknown <michel.igiraneza@auca.ac.rw>
2023-10-18 10:03:20 +0200

    Counting Century

---
+++ src/test/java/coding/codesignalanswers/CodesignalanswersApplicationTests.java
@@ -0,0 +1,13 @@
+package coding.codesignalanswers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodesignalanswersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
